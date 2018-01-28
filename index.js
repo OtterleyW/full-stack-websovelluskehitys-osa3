58e367dc9eec5b0e9b7f6f21c22e1e80378536ba
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-28 11:17:42 +0200

    Osa 3 tehtävä 3.11 lisää frontend nettiin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(bodyParser.json());
+app.use(express.static('build'))
 app.use(cors());
 app.use(
   morgan(function(tokens, req, res) {
