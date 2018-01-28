5c90959634f51829c7f3c9e9f5101e2cc7b4eb76
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-28 09:37:50 +0200

    Osa 3 tehtävä 3.8 konffaa morgan siten, että se näyttää HTTP-pyyntöjen mukana tulevan datan

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -4,7 +4,25 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+
+app.use(
+  morgan(function(tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.body(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms"
+    ].join(" ");
+  })
+);
+
+morgan.token("body", function(req, res) {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
   {
